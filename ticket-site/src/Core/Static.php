f539f6a3efc4782aed194a12cc466fedbf8c5a1d
parham minouian <79714647+pm1381@users.noreply.github.com>
2022-09-07 01:47:26 -0700

    removing unnesecarry files from Static.php

--- ticket-site/src/Core/Static.php
+++ ticket-site/src/Core/Static.php
@@ -14,7 +14,4 @@ define('REFRENCE_CONTROLLER', CONTROLLER . 'Refrence' . DIRECTORY_SEPRATOR);
 // adding all require onces
 require_once CORE    . 'Config.php';
 require_once LIBRARY . 'Function.php';
-require_once LIBRARY . 'JDF.php';
-require_once LIBRARY . 'simple_html_dom.php';
-require_once LIBRARY . 'Mobile_Detect.php';
 // and adding all files from library floder
